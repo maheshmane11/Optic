1b2c021208918555847bc8793ea535f56034e9ab
mahesh mane <maheshmane9211@gmail.com>
2017-08-28 16:02:09 +0530

    Vendor master design complete

--- Optic.Windows/Master/CreateVendorMaster.Designer.cs
+++ Optic.Windows/Master/CreateVendorMaster.Designer.cs
@@ -28,19 +28,280 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtOpBal = new System.Windows.Forms.TextBox();
+            this.txtMobileNumber = new System.Windows.Forms.TextBox();
+            this.txtOfficePhoneNo = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblOpBal = new System.Windows.Forms.Label();
+            this.lblMobileNumber = new System.Windows.Forms.Label();
+            this.lblOfficePhoneNumber = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblFrameMasterHeading = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.lblFaxNo = new System.Windows.Forms.Label();
+            this.lblEmailId = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtWebsite = new System.Windows.Forms.TextBox();
+            this.lblWebSite = new System.Windows.Forms.Label();
+            this.lblGST = new System.Windows.Forms.Label();
+            this.txtGstNo = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(292, 383);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(105, 23);
+            this.btnCancel.TabIndex = 43;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(159, 383);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 23);
+            this.btnSave.TabIndex = 42;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtOpBal
+            // 
+            this.txtOpBal.Location = new System.Drawing.Point(121, 329);
+            this.txtOpBal.Name = "txtOpBal";
+            this.txtOpBal.Size = new System.Drawing.Size(100, 20);
+            this.txtOpBal.TabIndex = 41;
+            // 
+            // txtMobileNumber
+            // 
+            this.txtMobileNumber.Location = new System.Drawing.Point(121, 286);
+            this.txtMobileNumber.Name = "txtMobileNumber";
+            this.txtMobileNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtMobileNumber.TabIndex = 40;
+            // 
+            // txtOfficePhoneNo
+            // 
+            this.txtOfficePhoneNo.Location = new System.Drawing.Point(121, 216);
+            this.txtOfficePhoneNo.Name = "txtOfficePhoneNo";
+            this.txtOfficePhoneNo.Size = new System.Drawing.Size(100, 20);
+            this.txtOfficePhoneNo.TabIndex = 39;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(121, 93);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(255, 20);
+            this.txtName.TabIndex = 38;
+            // 
+            // lblOpBal
+            // 
+            this.lblOpBal.AutoSize = true;
+            this.lblOpBal.Location = new System.Drawing.Point(31, 332);
+            this.lblOpBal.Name = "lblOpBal";
+            this.lblOpBal.Size = new System.Drawing.Size(42, 13);
+            this.lblOpBal.TabIndex = 37;
+            this.lblOpBal.Text = "Op Bal.";
+            // 
+            // lblMobileNumber
+            // 
+            this.lblMobileNumber.AutoSize = true;
+            this.lblMobileNumber.Location = new System.Drawing.Point(28, 289);
+            this.lblMobileNumber.Name = "lblMobileNumber";
+            this.lblMobileNumber.Size = new System.Drawing.Size(78, 13);
+            this.lblMobileNumber.TabIndex = 36;
+            this.lblMobileNumber.Text = "Mobile Number";
+            // 
+            // lblOfficePhoneNumber
+            // 
+            this.lblOfficePhoneNumber.AutoSize = true;
+            this.lblOfficePhoneNumber.Location = new System.Drawing.Point(28, 219);
+            this.lblOfficePhoneNumber.Name = "lblOfficePhoneNumber";
+            this.lblOfficePhoneNumber.Size = new System.Drawing.Size(89, 13);
+            this.lblOfficePhoneNumber.TabIndex = 35;
+            this.lblOfficePhoneNumber.Text = "Office Phone No.";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(28, 93);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 34;
+            this.lblName.Text = "Name";
+            // 
+            // lblFrameMasterHeading
+            // 
+            this.lblFrameMasterHeading.AutoSize = true;
+            this.lblFrameMasterHeading.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFrameMasterHeading.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblFrameMasterHeading.Location = new System.Drawing.Point(26, 30);
+            this.lblFrameMasterHeading.Name = "lblFrameMasterHeading";
+            this.lblFrameMasterHeading.Size = new System.Drawing.Size(208, 29);
+            this.lblFrameMasterHeading.TabIndex = 33;
+            this.lblFrameMasterHeading.Text = "Create New Vendor";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(28, 136);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 44;
+            this.lblAddress.Text = "Address";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(121, 119);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(255, 73);
+            this.txtAddress.TabIndex = 45;
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(28, 251);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(58, 13);
+            this.lblPhoneNumber.TabIndex = 46;
+            this.lblPhoneNumber.Text = "Phone No.";
+            // 
+            // lblFaxNo
+            // 
+            this.lblFaxNo.AutoSize = true;
+            this.lblFaxNo.Location = new System.Drawing.Point(259, 219);
+            this.lblFaxNo.Name = "lblFaxNo";
+            this.lblFaxNo.Size = new System.Drawing.Size(44, 13);
+            this.lblFaxNo.TabIndex = 47;
+            this.lblFaxNo.Text = "Fax No.";
+            // 
+            // lblEmailId
+            // 
+            this.lblEmailId.AutoSize = true;
+            this.lblEmailId.Location = new System.Drawing.Point(259, 255);
+            this.lblEmailId.Name = "lblEmailId";
+            this.lblEmailId.Size = new System.Drawing.Size(44, 13);
+            this.lblEmailId.TabIndex = 48;
+            this.lblEmailId.Text = "Email Id";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 248);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 49;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(322, 216);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 50;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(322, 252);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 51;
+            // 
+            // txtWebsite
+            // 
+            this.txtWebsite.Location = new System.Drawing.Point(322, 293);
+            this.txtWebsite.Name = "txtWebsite";
+            this.txtWebsite.Size = new System.Drawing.Size(100, 20);
+            this.txtWebsite.TabIndex = 52;
+            // 
+            // lblWebSite
+            // 
+            this.lblWebSite.AutoSize = true;
+            this.lblWebSite.Location = new System.Drawing.Point(259, 293);
+            this.lblWebSite.Name = "lblWebSite";
+            this.lblWebSite.Size = new System.Drawing.Size(48, 13);
+            this.lblWebSite.TabIndex = 53;
+            this.lblWebSite.Text = "WebSite";
+            // 
+            // lblGST
+            // 
+            this.lblGST.AutoSize = true;
+            this.lblGST.Location = new System.Drawing.Point(259, 332);
+            this.lblGST.Name = "lblGST";
+            this.lblGST.Size = new System.Drawing.Size(49, 13);
+            this.lblGST.TabIndex = 54;
+            this.lblGST.Text = "GST No.";
+            // 
+            // txtGstNo
+            // 
+            this.txtGstNo.Location = new System.Drawing.Point(322, 325);
+            this.txtGstNo.Name = "txtGstNo";
+            this.txtGstNo.Size = new System.Drawing.Size(100, 20);
+            this.txtGstNo.TabIndex = 55;
             // 
             // CreateVendorMaster
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(502, 430);
+            this.Controls.Add(this.txtGstNo);
+            this.Controls.Add(this.lblGST);
+            this.Controls.Add(this.lblWebSite);
+            this.Controls.Add(this.txtWebsite);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblEmailId);
+            this.Controls.Add(this.lblFaxNo);
+            this.Controls.Add(this.lblPhoneNumber);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtOpBal);
+            this.Controls.Add(this.txtMobileNumber);
+            this.Controls.Add(this.txtOfficePhoneNo);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblOpBal);
+            this.Controls.Add(this.lblMobileNumber);
+            this.Controls.Add(this.lblOfficePhoneNumber);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblFrameMasterHeading);
             this.Name = "CreateVendorMaster";
             this.Text = "New Vendor";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtOpBal;
+        private System.Windows.Forms.TextBox txtMobileNumber;
+        private System.Windows.Forms.TextBox txtOfficePhoneNo;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblOpBal;
+        private System.Windows.Forms.Label lblMobileNumber;
+        private System.Windows.Forms.Label lblOfficePhoneNumber;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblFrameMasterHeading;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.Label lblFaxNo;
+        private System.Windows.Forms.Label lblEmailId;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtWebsite;
+        private System.Windows.Forms.Label lblWebSite;
+        private System.Windows.Forms.Label lblGST;
+        private System.Windows.Forms.TextBox txtGstNo;
     }
 }
